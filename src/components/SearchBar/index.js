ce5c5f250556449381bfd5e3ef911f1fc75a2a12
Ochea <german.ochea@accenture.com>
2021-09-25 19:10:03 +0800

    continues deployment to netlify

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,42 @@
+import { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+import SearchIcon from '../../images/search-icon.svg';
+import { Wrapper, Content } from './SearchBar.styles';
+
+const SearchBar = ({ setSearchTerm }) => {
+  const [state, setState] = useState('');
+  const initial = useRef(true);
+
+  useEffect(() => {
+    if (initial.current) {
+      initial.current = false;
+      return;
+    }
+    const timer = setTimeout(() => {
+      setSearchTerm(state);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [setSearchTerm, state]);
+
+  return (
+    <Wrapper>
+      <Content>
+        <img src={SearchIcon} alt="search-icon" />
+        <input
+          type="text"
+          placeholder="Search Movie"
+          onChange={(e) => setState(e.currentTarget.value)}
+          value={state}
+        />
+      </Content>
+    </Wrapper>
+  );
+};
+
+SearchBar.propTypes = {
+  setSearchTerm: PropTypes.func,
+};
+
+export default SearchBar;
